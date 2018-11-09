8970012c25c8acbdd28dfd16cfd565c30135bfc3
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-09 10:12:32 -0500

    adding fruit and message

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(() => {
+    //set up vue instance = vm
+    //needs reference to wrapper --- el: '#app'
+    //reference message in the app (html)
+    const vm = new Vue({
+        el: '#app',
+
+        data : {
+            message : "Welcome to your first Vue app!",
+
+            targetURL : "http://google.ca",
+
+            deliciousFruit : [
+                { name: "apple", flavour: "tasty"},
+                { name: "orange", flavour: "sweet"},
+                { name: "grape", flavour: "juicy"}
+            ]
+        },
+
+        methods : {
+            logFruit(e){
+                console.log(e.currentTarget);
+            }
+        }
+    })
+
+})();
